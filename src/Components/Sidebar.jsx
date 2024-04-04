04b35bc117f409102d654bb6658757d5a6958a40
Sharad Singh <sharadsingh3030@gmail.com>
2024-04-04 04:09:29 -0400

    side bard anumation

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -9,49 +9,63 @@ import { CiSettings } from "react-icons/ci";
 import { IoIosInformationCircleOutline } from "react-icons/io";
 import { FaArrowCircleLeft } from "react-icons/fa";
 import { FaArrowCircleRight } from "react-icons/fa";
+import { TiWorld } from "react-icons/ti";
+import { FaAngleDown } from "react-icons/fa";
+import { FaAngleUp } from "react-icons/fa";
+
 const Sidebar = () => {
-    
-    const [menu,setMenu]=useState(true);
-    const [ScreenSize ,setScreenSize]=useState(window.innerWidth)
-    const handlehumber=()=>{
-       setMenu(!menu)
+
+    const [menu, setMenu] = useState(false);
+    const [ScreenSize, setScreenSize] = useState(window.innerWidth)
+    const handlehumber = () => {
+        setMenu(!menu)
     }
-   useEffect(()=>{
-    const handleResize=()=>{
-        setScreenSize(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenSize(window.innerWidth);
+        }
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+    useEffect(() => {
+        if (ScreenSize < 600) {
+            setMenu(false)
+        }
+        else if (ScreenSize >= 600) {
+            setMenu(true)
+        }
+    }, [ScreenSize])
+    const [clickDown, setClickDown] = useState(false)
+    const productDropdown = () => {
+        setClickDown(!clickDown)
+
+
     }
-    window.addEventListener('resize',handleResize);
-    return ()=>{
-        window.removeEventListener('resize',handleResize);
-    } 
-   },[]);
-   useEffect(()=>{
-         if(ScreenSize<600){
-            setMenu(!menu)
-         }
-         else if(ScreenSize>=600){
-            setMenu(!menu)
-         }
-   },[ScreenSize])
     return (
-        <div className={`h-screen flex relative  ${menu ? 'ml-0' : '-ml-64'} transition-all duration-100`} >
+        <div className={`h-screen flex relative  ${menu ? 'ml-0' : '-ml-64'} transition-all duration-1000`} >
             <div className='relative border rounded-r-[20px] h-[100vh] w-[250px] p-2 bg-white'>
                 <div className=' relative text-center p-[10px] mb-5'>
-                    <h1 className='font-bold text-[22px]' > World Model Hunt</h1>
+                    <div className='flex items-center' >
+                        <TiWorld size={"30px"} />
+                        <h1 className='font-bold text-[20px] ml-[3px]' > World Model Hunt</h1>
+                    </div>
                     {menu && <button onClick={handlehumber} className='absolute bg-white rounded-full -right-6 top-7 '><FaArrowCircleLeft size={35} /></button>}
-                    {!menu&&<button onClick={handlehumber} className='absolute bg-white rounded-full -right-[80px] top-7 '><FaArrowCircleRight size={35} /> </button>}
+                    {!menu && <button onClick={handlehumber} className='absolute bg-white rounded-full -right-[80px] top-7 '><FaArrowCircleRight size={35} /> </button>}
                 </div>
-                <div className= 'w-[100%]'>
+                <div className='w-[100%]'>
                     <button className='flex items-center w-[100%] text-center text-sm p-2 hover:bg-[#ede9fe] hover:rounded-xl hover:text-blue-700'>
                         <MdOutlineDashboardCustomize />
                         Dashboard
                     </button>
-                    <button className='flex items-center w-[100%] text-center text-sm p-2 active:bg-[#ede9fe] hover:rounded-xl hover:text-blue-700'>
+                    <button className='flex items-center w-[100%] text-center text-sm p-2 active:bg-[#ede9fe] hover:rounded-xl hover:text-blue-700' onClick={productDropdown}>
                         <LuInbox />
                         <p className='ml-1 mr-[100px]'>Products</p>
-                        <RxDropdownMenu />
+                        {clickDown? <FaAngleUp />:<FaAngleDown />}
                     </button>
-                    <div className='ml-3'>
+                    
+                        <div className={` ml-5 overflow-hidden transition-all duration-700 ${clickDown ? 'h-[100px]':'h-[0px]'} `}>
                             <div>
                                 <button className='flex items-center w-[100%] text-center text-sm p-2 hover:bg-[#ede9fe] hover:rounded-xl hover:text-blue-700'>
                                     <SlCalender />
@@ -73,7 +87,9 @@ const Sidebar = () => {
                                     <FaArrowRight />
                                 </button>
                             </div>
-                        </div>
+                    
+
+                    </div>
                 </div>
                 <hr className='mt-12' />
                 <div className='pt-8'>
